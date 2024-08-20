21f559901e6eb636649cce71e5b02dc59d845865
Giuliano Losa <giuliano@losa.fr>
2024-08-20 07:54:01 -0700

    use diff to show ctis

---
+++ show_cti.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# uses diff to display the latext counterexample to induction found by Apalache
+
+TRACE=$(find ./_apalache-out -type f -name "violation1.tla" -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f2-)
+
+PRE_FILE=$(mktemp)
+POST_FILE=$(mktemp)
+
+echo $TRACE > $PRE_FILE
+echo $TRACE > $POST_FILE
+awk -v RS="" 'NR==4' ${TRACE} | sed '1,2d' >> $PRE_FILE
+awk -v RS="" 'NR==5' ${TRACE} | sed '1,2d' >> $POST_FILE
+diff --side-by-side --width=$COLUMNS $PRE_FILE $POST_FILE | less -REX
+
+
